c4ae3d925ba66879151f52dc97bad0680c3dc7c8
Sergio Cano <canosergio90@gmail.com>
2022-09-03 02:12:15 +0200

    adding prittier and applying format
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 const basicTheme = createTheme({
   palette: {
-    mode: 'light'
-  }
-})
+    mode: "light",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ basicTheme }>
+    <ThemeProvider theme={basicTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
