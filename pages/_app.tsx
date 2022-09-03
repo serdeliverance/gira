1d292592c6cd784bc2b00f1c65b22d5ac5d15ad7
Sergio Cano <canosergio90@gmail.com>
2022-09-03 02:20:29 +0200

    extracting themes into dedicated folder
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme } from '../themes'
 
-const basicTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={basicTheme}>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
