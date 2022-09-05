0d49d56dafbd40440dcb96a7d0aedad3e40a2816
Sergio Cano <canosergio90@gmail.com>
2022-09-06 00:25:52 +0200

    index initial
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import { Typography } from '@mui/material'
+import { Card, CardContent, CardHeader, Grid } from '@mui/material'
 import type { NextPage } from 'next'
 import { Layout } from '../components/layouts'
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Home Page</Typography>
+    <Layout title='Home Open Gira'>
+      <Grid container spacing={ 2 }>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Pendings'></CardHeader>
+            <CardContent>
+            </CardContent>
+          </Card>
+        </Grid>
+        
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='In Progress'></CardHeader>
+          </Card>
+        </Grid>
+        
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Done'></CardHeader>
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
