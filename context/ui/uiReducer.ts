02a3cac56bca2715c980ddf5b7b08e26c3e9dae3
Sergio Cano <canosergio90@gmail.com>
2022-09-05 10:24:50 +0200

    reducer
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- context/ui/uiReducer.ts
+++ context/ui/uiReducer.ts
@@ -1,1 +1,22 @@
-export const uiReducer = () => {}
+import { UIState } from './UIProvider'
+
+type UIAction =
+| { type: 'UI - Open Sidebar' }
+| { type: 'UI - Close Sidebar' }
+
+export const uiReducer = ( state: UIState, action: UIAction ): UIState => {
+    switch (action.type) {
+        case 'UI - Open Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: true
+            }
+        case 'UI - Open Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: false
+            }
+        default:
+            return state
+    }
+}
