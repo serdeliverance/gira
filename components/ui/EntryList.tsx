5d28d84cb422f3e93abe264007007693a14fe565
Sergio Cano <canosergio90@gmail.com>
2022-09-09 21:50:40 +0200

    adding styling when dragging
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -5,6 +5,8 @@ import { UIContext } from '../../context/ui'
 import { EntryStatus } from '../../interfaces'
 import { EntryCard } from './EntryCard'
 
+import styles from './EntryList.module.css'
+
 interface Props {
   status: EntryStatus
 }
@@ -30,6 +32,7 @@ export const EntryList: FC<Props> = ({ status }) => {
     <div
       onDrop={ onDropEntry }
       onDragOver={ allowDrop }
+      className= { isDragging ? styles.dragging : ''}
     >
         <Paper sx={{ height: 'calc(100vh - 180px)', overflow: 'scroll', backgroundColor: 'transparent', padding: '1px 5px' }}>
             <List sx={{ opacity: isDragging ? 0.2 : 1, transition: 'all .4s' }}>
