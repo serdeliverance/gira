62aa47492fb76eba90ec9dd51094e1ef9cbf370b
Sergio Cano <canosergio90@gmail.com>
2022-09-15 22:15:26 +0200

    update entries
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { Entry, IEntry } from '../../../models'
+
+type Data = 
+    | { message: string }
+    | IEntry
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    const { id } = req.query
+
+    if ( !mongoose.isValidObjectId( id )) {
+
+        return res.status(400).json({message: `invalid id: ${id}`})
+    }
+
+    switch ( req.method ) {
+        case 'PUT':
+            return updateEntry(req, res)
+        default:
+            return res.status(400)
+    }
+
+}
+
+const updateEntry = async( req: NextApiRequest, res: NextApiResponse<Data>) => {
+    
+    const { id } = req.query
+
+    await db.connect()
+
+    const entryToUpdate = await Entry.findById(id)
+
+    if ( !entryToUpdate ) {
+        await db.disconnect()
+        return res.status(404).json({message: `not found entry with id: ${id}`})
+    }
+
+    const {
+        description = entryToUpdate.description,
+        status = entryToUpdate.status
+    } = req.body
+
+    const updatedEntry = await Entry.findByIdAndUpdate(id, { description , status }, { runValidators: true, new: true })
+
+    res.status(200).json(updatedEntry!)
+}
