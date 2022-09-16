d579c6b649c957f3bc59100b6ff5e317c4f1c141
Sergio Cano <canosergio90@gmail.com>
2022-09-16 11:37:54 +0200

    calling update endpoint from ui
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -27,7 +27,14 @@ export const EntriesProvider:FC<Props> = ({ children }) => {
         dispatch({ type: 'Add-Entry', payload: data })
     }
 
-    const updateEntry = ( entry: Entry ) => dispatch({ type: 'Entry-Updated', payload: entry})
+    const updateEntry = async ( {_id, description, status}: Entry ) => {
+        try {
+            const { data } = await entriesApi.put<Entry>(`/entries/${ _id }`, { description, status })
+            dispatch({ type: 'Entry-Updated', payload: data})            
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const refreshEntries = async() => {
         const { data } = await entriesApi.get<Entry[]>('/entries')
