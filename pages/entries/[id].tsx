13041824514edd270d580ed0d3235a5bf6e9689c
Sergio Cano <canosergio90@gmail.com>
2022-09-18 21:20:56 +0200

    update entry form - initial
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -3,10 +3,26 @@ import { Layout } from "../../components/layouts"
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
 import { EntryStatus } from "../../interfaces"
 import DeleteIcon from '@mui/icons-material/Delete'
+import { ChangeEvent, useState } from "react"
 
 const STATUS: EntryStatus[] = ['pending', 'in-progress', 'finished']
 
 export const EntryPage = () => {
+
+    const [inputValue, setInputValue] = useState('')
+    const [status, setStatus] = useState<EntryStatus>('pending')
+    const [touched, setTouched] = useState(false)
+
+    const onInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+    }
+
+    const onStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setStatus(event.target.value as EntryStatus)
+    }
+
+    const onSave = () => {}
+
     return (
         <Layout>
             <Grid container justifyContent='center' sx={{marginTop: 2}}>
@@ -21,10 +37,16 @@ export const EntryPage = () => {
                             autoFocus
                             multiline
                             label="New entry"
+                            value={inputValue}
+                            onChange={onInputValueChange}
                         />
                         <FormControl>
                             <FormLabel>State:</FormLabel>
-                            <RadioGroup row>
+                            <RadioGroup 
+                                row
+                                value={status}
+                                onChange={onStatusChange}
+                            >
                                 {
                                     STATUS.map(option => (
                                         <FormControlLabel 
@@ -44,6 +66,7 @@ export const EntryPage = () => {
                             startIcon={<SaveOutlinedIcon />}
                             variant="contained"
                             fullWidth
+                            onClick={onSave}
                         >
                             Save
                         </Button>
